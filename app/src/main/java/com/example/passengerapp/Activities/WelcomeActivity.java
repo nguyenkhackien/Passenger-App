608a13f44db41c060a8c74f23f7cde7535dbec9c
nguyenkhackien <20020679@vnu.edu.vn>
2024-05-12 14:02:19 +0700

    first commit

---
+++ app/src/main/java/com/example/passengerapp/Activities/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.passengerapp.Activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.passengerapp.R;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        FirebaseDatabase.getInstance().getReference().child("test").setValue(1);
+    }
+}
